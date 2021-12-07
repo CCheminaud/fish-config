754c1fa693c129161959dd27cf23b7e824c08f78
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2021-12-07 11:56:30 +0100

    feat: use NVM to load the right Node.js version for current directory

--- config.fish
+++ config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Use NVM to load the right Node.js
+# https://github.com/nvm-sh/nvm
+load_nvm
+
 # Load Starship prompt
 # https://starship.rs/
 set -x STARSHIP_CONFIG ~/.config/starship/config.toml
