4ae1f5f5aa9c17e3ee3b98644ebf1c33d5fe1e05
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2021-12-07 11:29:21 +0100

    feat(config.fish): setup starship prompt

--- config.fish
+++ config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Load Starship prompt
+# https://starship.rs/
+set -x STARSHIP_CONFIG ~/.config/starship/config.toml
+starship init fish | source
