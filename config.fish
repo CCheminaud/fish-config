c472b73dba4da48ab616f4d8383caa94be3eb0a4
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2021-12-08 08:59:11 +0100

    feat(config.fish): define EDITOR environment variable

--- config.fish
+++ config.fish
@@ -2,6 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Editors
+if type -q nvim
+    set -gx EDITOR nvim
+else
+    set -gx EDITOR vim
+end
+
 # Use NVM to load the right Node.js
 # https://github.com/nvm-sh/nvm
 load_nvm
