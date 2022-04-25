6cf6a5ac0a6a85303a2289f89cae6ec7d2d04fe6
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2022-04-25 18:25:21 +0200

    fix(functions): fix name of function aws_ssm_start_session

--- functions/aws_ssm_start_session.fish
+++ functions/aws_ssm_start_session.fish
@@ -1,3 +1,3 @@
-function ssm_start_session -d "Start a new SSM session" -a ec2_id
+function aws_ssm_start_session -d "Start a new SSM session" -a ec2_id
     aws ssm start-session --target $ec2_id
 end
